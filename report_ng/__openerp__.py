db58c740e33a2ebe9186ab63783878e1b4b2fbd3
Nicolas JEUDY <nicolas@sudokeys.com>
2015-12-30 17:02:32 +0100

    fix: add ir.rule to be able to have one template per company

--- report_ng/__openerp__.py
+++ report_ng/__openerp__.py
@@ -1,18 +1,23 @@
+# -*- coding: utf-8 -*-
+
 {
-    'name': 'report_ng',
-    'version': '0.1',
-    'category': 'Tools',
-    'summary': 'Next generation report utils',
-    'description': """Add commun report action / setup""",
-    'depends': ['base', 'report'],
-    'data': [
-        'views/report_style_layout.xml',
+    "name": 'report_ng',
+    "description": u"""Add commun report action / setup""",
+    "version": "0.1",
+    "depends": [
+        'base',
+        'report',
+    ],
+    "author": "None",
+    "category": "Tools",
+    "installable": True,
+    "data": [
         'views/ir_ui_view_view.xml',
+        'views/report_style_layout.xml',
         'views/layout_header_view.xml',
         'views/layout_footer_view.xml',
         'views/res_company_view.xml',
         'views/res_partner_templates_view.xml',
+        'data/ir_rule.xml',
     ],
-    'installable': True,
-    'application': False,
 }
